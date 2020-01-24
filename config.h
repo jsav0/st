e407e485d9794bbd519044f629106610a1daf670
jsav0 <jas.savage@gmail.com>
2020-01-24 13:44:36 -0500

    added patch: copyurl

--- config.h
+++ config.h
@@ -193,6 +193,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD, 		XK_U,		externalpipe,	{ .v = openurlcmd } },
+        { MODKEY,               XK_l,           copyurl,        {.i =  0} },
 };
 
 /*
